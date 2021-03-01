4d204b85729ac90014b7696cc3b248fa2edb0cd0
TheLolCoder2007 <52106896+TheLolCoder2007@users.noreply.github.com>
2021-03-01 18:45:24 +0100

    language pack English installed
    language pack English version alpha0.1

---
+++ newserver/en.lang.ps1
@@ -0,0 +1,4 @@
+﻿$loading = "loading functions & modules, wait a moment..."
+$serverPRTquestion = "what is your server port?"
+$serverPRTskip = "server.properties is skipped"
+$sameAsServerPRT = "the same as server port"
