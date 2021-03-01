285db5770097135a063207cf9c28a2d7d20d3e32
TheLolCoder2007 <52106896+TheLolCoder2007@users.noreply.github.com>
2021-03-01 17:36:13 +0100

    Delete removesessions.ps1

--- removesessions/removesessions.ps1
+++
@@ -1,8 +0,0 @@
-﻿$sshsessions = Get-SSHSession
-$SFTPSessions = Get-SFTPSession
-if ($sshsessions.Connected) {
-Remove-SSHSession -SessionId 0
-}
-if ($SFTPSessions.Connected) {
-Remove-SFTPSession -SessionId 0
-}
