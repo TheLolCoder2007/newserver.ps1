4827a12a6458416eeda82a721846814164da9cdd
TheLolCoder2007 <52106896+TheLolCoder2007@users.noreply.github.com>
2021-03-11 18:47:48 +0100

    update langs
    add default and reverse server.jar question

--- assets/langs/template.lang.ps1
+++ assets/langs/template.lang.ps1
@@ -13,19 +13,19 @@ $qportquestion = ""#what is your query port? (it will be the same as the server 
 $gamemodequestion = ""#which gamemode do you want? (0=survivor, 1=creative, 2=adventure, 3=spectator)
 $gamemodefault = ""#you didn't entered an number in range from 0-3. The script will now quit
 $rconpassquestion = ""#type your secret Remote Control password here
-$eula_txtquestion = ""#do you already have an eula.txt?(y/n)
+$eula_txtquestion = ""#do you already have an eula.txt?(y/n)(default=n)
 $y = ""#y
 $n = ""#n
 $eula_txtskip = ""#skipping eula.txt
 $eula_txtfault = ""#not answered y/n. script will now quit.
-$start_shquestion = ""#do you have an start.sh?(y/n)
+$start_shquestion = ""#do you have an start.sh?(y/n)(default=n)
 $start_shskip = ""#start.sh is skipped
 $start_shfault = $eula_txtfault
 $compnamequestion = ""#What is your computer name/ip adress for the server where you want to host the server on? This need to be an linux host.
 $usernamequestion = ""#what is your username for
 $passwordTwice = ""#You need to fill in your password twice
 $serverprtexists = ""#your server port already exist. Program will now quit.
-$downloadquestion = ""#do you need to download your server.jar?(y/n)
+$downloadquestion = ""#do you have a server.jar?(y/n)(default=n)
 $serverjarskipped = ""#server.jar download skipped
 $serverjarfault = $eula_txtfault
 $jarfilechoose = ""#spigot, bukkit or forge (bukkit recommended)
